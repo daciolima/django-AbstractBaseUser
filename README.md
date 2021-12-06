9de5954eddacf51beed64f073c41a1a979ad50d7
Dacio Lima <contato.dacio@gmail.com>
2021-12-05 23:33:54 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+### Modelo django usando AbstractBaseUser
+
+Ver arquivos:
+- admin/backends.py
+- admin/settings.py
+- core/models.py
+- core/admin.py
---
+++ core/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from core.models import Account
+
+
+
+class AccountAdmin(UserAdmin):
+    list_display = ('email', 'username', 'created_at', 'updated_at', 'is_admin', 'is_staff')
+    search_fields = ('email', 'username')
+    readonly_fields = ('id', 'created_at', 'updated_at')
+
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
+
+admin.site.register(Account, AccountAdmin)
+
+
+
